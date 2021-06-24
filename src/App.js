0aef740f3f9795f4c0231e8b810a9027fd64f250
=Rob Franke <robwfranke@gmail.com>
2021-06-24 17:49:09 +0200

    wil nog niet compilen!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import FilesUpload from "./components/FilesUpload";
 
 function App() {
-  return (
-      <div className="container" style={{ width: "600px" }}>
-        <div className="my-3">
-          <h3>bezkoder.com</h3>
-          <h4>React Hooks Multiple Files Upload</h4>
-        </div>
+    return (
+        <div className="container" style={{ width: "600px" }}>
+            <div className="my-3">
+                <h3>bezkoder.com</h3>
+                <h4>React Hooks Multiple Files Upload</h4>
+            </div>
 
-        <FilesUpload />
-      </div>
-  );
+            <FilesUpload />
+        </div>
+    );
 }
 
 export default App;
