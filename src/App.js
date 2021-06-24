24987c8ce0cf88664f166c021e7a96c1e8d7e92c
=Rob Franke <robwfranke@gmail.com>
2021-06-24 18:04:55 +0200

    nu kun je files sturen naar backend, nog uitzoeken waarom het werkt...

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Test from "./components/Test"
 import FilesUpload from "./components/FilesUpload";
 
 function App() {
     return (
-        <div className="container" style={{ width: "600px" }}>
+        <div className="container" style={{width: "600px"}}>
             <div className="my-3">
                 <h3>bezkoder.com</h3>
                 <h4>React Hooks Multiple Files Upload</h4>
+
             </div>
 
-            <FilesUpload />
-        </div>
-    );
-}
 
-export default App;
+            <FilesUpload/>
+            <Test/>
+
+        </div>
+
+
+                );
+                }
+
+                export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+function Test(){
+
+
+
+    return(
+     <div>op test</div>
+
+    );
+}
+
+
+export default Test;
